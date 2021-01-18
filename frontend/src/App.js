e246ebb2b367781e756a5a153d4554f74ea41645
Leonardo Alves(leo-pro) <leoroberto.alves@hotmail.com>
2021-01-18 12:43:42 -0300

    Start project

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Desafio de ReactJS para a vaga na MarketMidia!</h1>
+    </div>
+  );
+}
+
+export default App;
